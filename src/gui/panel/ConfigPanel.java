86ffbfb5cd7a392d1f9d7aa1f82c606186010162
lqkx <wwl9679@qq.com>
2025-06-03 20:22:59 +0800

    在设置面板加上监听器

--- src/gui/panel/ConfigPanel.java
+++ src/gui/panel/ConfigPanel.java
@@ -1,5 +1,6 @@
 package gui.panel;
  
+import gui.listener.ConfigListener;
 import util.ColorUtil;
 import util.GUIUtil;
 
@@ -7,42 +8,50 @@ import javax.swing.*;
 import java.awt.*;
  
 public class ConfigPanel extends JPanel {
-    static{
+    static {
         GUIUtil.useLNF();
     }
+ 
     public static ConfigPanel instance = new ConfigPanel();
  
     JLabel lBudget = new JLabel("本月预算(￥)");
     public JTextField tfBudget = new JTextField("0");
-     
+ 
     JLabel lMysql = new JLabel("Mysql安装目录");
     public JTextField tfMysqlPath = new JTextField("");
  
     JButton bSubmit = new JButton("更新");
  
     public ConfigPanel() {
-        GUIUtil.setColor(ColorUtil.grayColor, lBudget,lMysql);
+        GUIUtil.setColor(ColorUtil.grayColor, lBudget, lMysql);
         GUIUtil.setColor(ColorUtil.blueColor, bSubmit);
-         
-        JPanel pInput =new JPanel();
+ 
+        JPanel pInput = new JPanel();
         JPanel pSubmit = new JPanel();
-        int gap =40;
-        pInput.setLayout(new GridLayout(4,1,gap,gap));
-         
+        int gap = 40;
+        pInput.setLayout(new GridLayout(4, 1, gap, gap));
+ 
         pInput.add(lBudget);
         pInput.add(tfBudget);
         pInput.add(lMysql);
         pInput.add(tfMysqlPath);
-        this.setLayout(new BorderLayout());
-        this.add(pInput,BorderLayout.NORTH);
-         
+ 
         pSubmit.add(bSubmit);
-        this.add(pSubmit,BorderLayout.CENTER);
+ 
+        this.setLayout(new BorderLayout());
+        this.add(pInput, BorderLayout.NORTH);
+        this.add(pSubmit, BorderLayout.CENTER);
          
+        addListener();
     }
  
     public static void main(String[] args) {
         GUIUtil.showPanel(ConfigPanel.instance);
+    }
+     
+    public void addListener() {
+        ConfigListener l =new ConfigListener();
+        bSubmit.addActionListener(l);
     }
      
 }
