15779f6c4652d391b77546af1a8784701b89d295
lqkx <wwl9679@qq.com>
2025-06-03 11:14:17 +0800

    恢复面板

---
+++ src/gui/panel/RecoverPanel.java
@@ -0,0 +1,27 @@
+package gui.panel;
+ 
+import util.ColorUtil;
+import util.GUIUtil;
+
+import javax.swing.*;
+ 
+public class RecoverPanel extends JPanel {
+    static{
+        GUIUtil.useLNF();
+    }
+    public static RecoverPanel instance = new RecoverPanel();
+ 
+    JButton bRecover =new JButton("»Ö¸´");
+ 
+    public RecoverPanel() {
+ 
+        GUIUtil.setColor(ColorUtil.blueColor, bRecover);
+        this.add(bRecover);
+         
+    }
+ 
+    public static void main(String[] args) {
+        GUIUtil.showPanel(RecoverPanel.instance);
+    }
+ 
+}
