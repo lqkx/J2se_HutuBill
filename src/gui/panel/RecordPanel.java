f23e7ca547d0e5f32a941f56ee749e6f4ee00617
lqkx <wwl9679@qq.com>
2025-06-03 20:57:00 +0800

    加上记录监听器

--- src/gui/panel/RecordPanel.java
+++ src/gui/panel/RecordPanel.java
@@ -1,22 +1,18 @@
 package gui.panel;
  
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.util.Date;
- 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
- 
-import org.jdesktop.swingx.JXDatePicker;
- 
+import entity.Category;
+import gui.listener.RecordListener;
 import gui.model.CategoryComboBoxModel;
+import org.jdesktop.swingx.JXDatePicker;
+import service.CategoryService;
 import util.ColorUtil;
 import util.GUIUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
  
-public class RecordPanel extends JPanel{
+public class RecordPanel extends WorkingPanel {
     static{
         GUIUtil.useLNF();
     }
@@ -30,7 +26,7 @@ public class RecordPanel extends JPanel{
     public JTextField tfSpend = new JTextField("0");
  
     public CategoryComboBoxModel cbModel = new CategoryComboBoxModel();
-    public JComboBox<String> cbCategory = new JComboBox<>(cbModel);
+    public JComboBox<Category> cbCategory = new JComboBox<>(cbModel);
     public JTextField tfComment = new JTextField();
     public JXDatePicker datepick = new JXDatePicker(new Date());
      
@@ -58,10 +54,39 @@ public class RecordPanel extends JPanel{
         this.setLayout(new BorderLayout());
         this.add(pInput,BorderLayout.NORTH);
         this.add(pSubmit,BorderLayout.CENTER);
+         
+        addListener();
     }
  
     public static void main(String[] args) {
         GUIUtil.showPanel(RecordPanel.instance);
     }
+ 
+    public Category getSelectedCategory(){
+        return (Category) cbCategory.getSelectedItem();
+    }
+ 
+    @Override
+    public void updateData() {
+        cbModel.cs = new CategoryService().list();
+        cbCategory.updateUI();
+        resetInput();
+        tfSpend.grabFocus();
+    }
      
+    public void resetInput(){
+        tfSpend.setText("0");
+        tfComment.setText("");
+        if(0!=cbModel.cs.size())
+            cbCategory.setSelectedIndex(0);
+        datepick.setDate(new Date());
+    }   
+ 
+    @Override
+    public void addListener() {
+        // TODO Auto-generated method stub
+        RecordListener listener = new RecordListener();
+        bSubmit.addActionListener(listener);
+    }
+ 
 }
