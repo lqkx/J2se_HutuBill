e3163b3653778940d57e58f7df8528f5cbaf70d4
lqkx <wwl9679@qq.com>
2025-06-03 21:29:43 +0800

    月消费报表

--- src/gui/panel/ReportPanel.java
+++ src/gui/panel/ReportPanel.java
@@ -1,30 +1,44 @@
 package gui.panel;
  
+import entity.Record;
+import service.ReportService;
 import util.ChartUtil;
-import util.GUIUtil;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+
+import static util.GUIUtil.showPanel;
  
-public class ReportPanel extends JPanel {
-    static {
-        GUIUtil.useLNF();
-    }
- 
+public class ReportPanel extends WorkingPanel {
     public static ReportPanel instance = new ReportPanel();
  
-    public JLabel l = new JLabel();
+    JLabel l = new JLabel();
  
     public ReportPanel() {
         this.setLayout(new BorderLayout());
-        Image i =ChartUtil.getImage(400, 300);
-        ImageIcon icon= new ImageIcon(i);
+        List<Record> rs = new ReportService().listThisMonthRecords();
+        Image i = ChartUtil.getImage(rs, 400, 300);
+        ImageIcon icon = new ImageIcon(i);
         l.setIcon(icon);
         this.add(l);
+        addListener();
     }
  
     public static void main(String[] args) {
-        GUIUtil.showPanel(ReportPanel.instance);
+        showPanel(ReportPanel.instance);
     }
  
+    @Override
+    public void updateData() {
+        List<Record> rs = new ReportService().listThisMonthRecords();
+        Image i = ChartUtil.getImage(rs, 350, 250);
+        ImageIcon icon = new ImageIcon(i);
+        l.setIcon(icon);
+    }
+ 
+    @Override
+    public void addListener() {
+ 
+    }
 }
