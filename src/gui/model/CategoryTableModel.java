ed4056520cd7d649664c8c28e9c850f6c2dc2a06
lqkx <wwl9679@qq.com>
2025-06-03 20:29:50 +0800

    在分类面板加上监听器

--- src/gui/model/CategoryTableModel.java
+++ src/gui/model/CategoryTableModel.java
@@ -1,78 +1,48 @@
 package gui.model;
- 
-import java.util.ArrayList;
+  
+import entity.Category;
+import service.CategoryService;
+
+import javax.swing.table.AbstractTableModel;
 import java.util.List;
- 
-import javax.swing.event.TableModelListener;
-import javax.swing.table.TableModel;
- 
-public class CategoryTableModel implements TableModel{
- 
-    String[] columnNames = new String[]{"分类名称","消费次数"};
-    List<String> cs = new ArrayList<>();
-     
-    public CategoryTableModel(){
-        cs.add("餐饮");
-        cs.add("交通");
-        cs.add("住宿");
-        cs.add("话费");
-    }
-    @Override
-    public int getRowCount() {
-        // TODO Auto-generated method stub
+  
+public class CategoryTableModel extends AbstractTableModel {
+  
+    String[] columnNames = new String[] { "分类名称", "消费次数"};
+  
+    // 使用从Service返回的List作为TableModel的数据
+  
+    public List<Category> cs = new CategoryService().list();
+  
+     public int getRowCount() {
+         
         return cs.size();
     }
- 
-    @Override
+  
     public int getColumnCount() {
-        // TODO Auto-generated method stub
+         
         return columnNames.length;
     }
- 
-    @Override
+  
     public String getColumnName(int columnIndex) {
-        // TODO Auto-generated method stub
+         
         return columnNames[columnIndex];
     }
- 
-    @Override
-    public Class<?> getColumnClass(int columnIndex) {
-        // TODO Auto-generated method stub
-        return String.class;
-    }
- 
-    @Override
+  
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
         return false;
     }
- 
-    @Override
+  
+    // 先通过cs.get(rowIndex)获取行对应的Category对象
+    // 然后根据columnIndex返回对应的属性
     public Object getValueAt(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
-        if(0==columnIndex)
-            return cs.get(rowIndex);
-        if(1==columnIndex)
-            return 0;
+        Category h = cs.get(rowIndex);
+        if (0 == columnIndex)
+            return h.name;
+        if (1 == columnIndex)
+            return h.recordNumber;
+ 
         return null;
     }
- 
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
-         
-    }
- 
-    @Override
-    public void addTableModelListener(TableModelListener l) {
-        // TODO Auto-generated method stub
-         
-    }
- 
-    @Override
-    public void removeTableModelListener(TableModelListener l) {
-        // TODO Auto-generated method stub
-         
-    }
- 
+  
 }
